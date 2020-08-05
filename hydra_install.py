a483501087818534e0f1de5265d6572bfed50851
Hydra-X <68790488+Hydra-X@users.noreply.github.com>
2020-08-05 09:19:27 -0400

    Add files via upload

---
+++ hydra_install.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os
+
+os.system("apt-get install dnsenum")
+os.system("apt-get install fierce")
+os.system("apt-get install dnsrecon")
+os.system("apt-get install dnsmap")
+os.system("apt-get install goofile")
+os.system("apt-get install dnswalk")
+os.system("apt-get install dnsdict6")
+os.system("apt-get install dirb")
+os.system("apt-get install wafw00f")
+os.system("apt-get install python")
+os.system("apt-get install python3")
+os.system("apt-get install dmitry")
+os.system("apt-get install msfconsole")
+os.system("apt-get install ")
+os.system("apt-get install fierce")
+os.system("apt-get install blindelephant.py")
+os.system("apt-get install figlet")
+os.system("apt-get install tor")
+os.system("clear")
